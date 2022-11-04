3cb8ae4ca2b335c392cdb0f8ed95407f741d8de6
Will <trainee.william.simms@sigmalabs.co.uk>
2022-11-04 18:29:23 +0000

    all challenges completed

--- scrabble.py
+++ scrabble.py
@@ -1,12 +1,12 @@
 import random
 
-tile_dict = {'one_pt': ['E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U'],
+tile_dict = {'one_pt': ['E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U'], # dict keys integers?
 'two_pt': ['D', 'G'],
 'three_pt': ['B', 'C', 'M', 'P'],
 'four_pt': ['F', 'H', 'V', 'W', 'Y'],
 'five_pt': ['K'],
 'eight_pt': ['J', 'X'],
-'ten_pt': ['Q', 'Z,']}
+'ten_pt': ['Q', 'Z']}
 
 tile_distribution = {'twelve_occ': ['E'],
 'nine_occ': ['A', 'I'],
@@ -18,11 +18,10 @@ tile_distribution = {'twelve_occ': ['E'],
 'one_tile': ['K', 'J', 'X', 'Q', 'Z']
 }
 
-
 def score_for_word(word):
     word_list = list(word.upper())
     score = 0
-    for letter in word_list:
+    for letter in word_list: # if keys were integers, loop through tile dict for each tile dict, if letter in tile dict , score += key for tile dict
         if letter in tile_dict['one_pt']:
             score += 1
         elif letter in tile_dict['two_pt']:
@@ -80,7 +79,7 @@ def shuffle_bag(bag):
     random.shuffle(bag)
     return bag
 
-def generate_rack(shuffled_bag):
+def generate_rack(shuffled_bag): # slice from 0 to 7 from shuffled bag
     rack = []
     for i in range(7):
         rack.append(shuffled_bag.pop())
@@ -90,14 +89,13 @@ def check_valid_word(word):
     is_valid = 0
     with open('dictionary.txt', 'r') as f:
         for line in f:
-            if line.strip('\n') == word:
+            if line.strip('\n') == word: # send to Seb
                 is_valid = 1
     return is_valid     
 
 def find_words_in_rack(rack):
     words_in_rack = []
     with open('dictionary.txt', 'r') as f:
-    # Accounting for duplicates: loop through each line of the dictionary and check if the word can be made
         for line in f:
             rack_copy = rack.copy()   # copy of the rack to work with 
             split_line = list(line.strip('\n').upper()) # list version of the current word to work with 
@@ -105,17 +103,58 @@ def find_words_in_rack(rack):
             for letter in split_line:   # loop through each letter in the line
                 if letter in rack_copy: # if the letter is in the rack copy, is in rack is true and we remove that letter from the rack copy
                     is_in_rack = 1
-                    rack_copy.remove(letter)
+                    rack_copy.remove(letter) # this is where the magic happens...
                 else:                   # if the letter is not in the rack copy, we know the word can't be made so we move on to the next line
                     is_in_rack = 0
                     break
             if(is_in_rack):
-                words_in_rack.append(line.strip('\n').upper())        
+                words_in_rack.append(line.strip('\n').upper()) # consider converting dict to list and stripping all \n.     
     return words_in_rack
     
-def find_longest_word_in_rack(rack):
+def find_longest_words_in_rack(rack):
     words_in_rack = find_words_in_rack(rack)
-    return max(words_in_rack, key=len)
-    
-print(find_words_in_rack(['A', 'C', 'E', 'R', 'P', 'Z', 'X']))     
-print(find_longest_word_in_rack(['A', 'C', 'E', 'R', 'P', 'Z', 'X']))
+    max_length = len(max(words_in_rack, key=len))
+    longest_words_in_rack = list(filter(lambda a: len(a) == max_length, words_in_rack))
+    return {max_length: longest_words_in_rack}
+
+def find_highest_score_in_rack(rack):
+    words_in_rack = find_words_in_rack(rack)
+    scores_in_rack = list(map(score_for_word, words_in_rack))
+    max_score_in_rack = max(scores_in_rack)
+    return max_score_in_rack
+
+def find_max_score_words_in_rack(rack):
+    words_in_rack = find_words_in_rack(rack)
+    highest_scoring_words = [word for word in words_in_rack if(score_for_word(word) == find_highest_score_in_rack(rack))]
+    return {score_for_word(highest_scoring_words[0]): highest_scoring_words}
+
+def find_max_score_word_triple(rack): # this doesn't work, ZAX is better..
+    words_in_rack = find_words_in_rack(rack)
+    max_triple_score = 0
+    best_word = ''
+    for word in words_in_rack:
+        for letter in list(word):
+            score = (score_for_word(word[word.index(letter)]) * 2) + score_for_word(word)
+            if score > max_triple_score:
+                best_word = word
+                max_triple_score = score
+    return {best_word: max_triple_score}
+
+
+
+# loop through words in rack
+#       nest loop through each letter of the word
+#           calculate points for word if that letter is a triple.
+#           if points for the word > max_triple_score:
+#                max_triple_score = points for the word
+
+
+# test code:
+
+test_rack = ['A', 'C', 'E', 'R', 'X', 'Y', 'Z']
+
+print(find_words_in_rack(test_rack))     
+print(find_longest_words_in_rack(test_rack))
+print(find_highest_score_in_rack(test_rack))
+print(find_max_score_words_in_rack(test_rack))
+print(find_max_score_word_triple(test_rack))
