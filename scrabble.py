444f928dcaef01331e01a73a70d72a2ece9d33d2
Will <trainee.william.simms@sigmalabs.co.uk>
2022-11-04 11:31:51 +0000

    created points for word function

--- scrabble.py
+++ scrabble.py
@@ -8,6 +8,22 @@ tile_dict = {'one_pt': ['E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U'],
 
 
 def score_for_word(word):
+    word_list = list(word.upper())
     score = 0
-    for letter in word:
-        if tile in
+    for letter in word_list:
+        if letter in tile_dict['one_pt']:
+            score += 1
+        elif letter in tile_dict['two_pt']:
+            score += 2
+        elif letter in tile_dict['three_pt']:
+            score += 3
+        elif letter in tile_dict['four_pt']:
+            score += 4
+        elif letter in tile_dict['five_pt']:
+            score += 5
+        elif letter in tile_dict['eight_pt']:
+            score += 8
+        elif letter in tile_dict['ten_pt']:
+            score += 10
+    return score
+
