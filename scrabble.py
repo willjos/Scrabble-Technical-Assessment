5dc840da918b49f7a10fbe1a0db174579df11fee
Will <trainee.william.simms@sigmalabs.co.uk>
2022-11-04 11:39:21 +0000

    added occurences dict and started generate bag and rack functions

--- scrabble.py
+++ scrabble.py
@@ -6,6 +6,16 @@ tile_dict = {'one_pt': ['E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U'],
 'eight_pt': ['J', 'X'],
 'ten_pt': ['Q', 'Z,']}
 
+tile_distribution = {'twelve_occ': ['E'],
+'nine_occ': ['A', 'I'],
+'eight_occ': ['O'],
+'six_occ': ['N', 'R', 'T'],
+'four_occ': ['L', 'S', 'U', 'D'],
+'three_occ': ['G'],
+'two_occ': ['B', 'C', 'M', 'P', 'F', 'H', 'V', 'W', 'Y'],
+'one_tile': ['K', 'J', 'X', 'Q', 'Z']
+}
+
 
 def score_for_word(word):
     word_list = list(word.upper())
@@ -26,4 +36,12 @@ def score_for_word(word):
         elif letter in tile_dict['ten_pt']:
             score += 10
     return score
+
+def generate_bag():
+    
+
+def generate_rack():
+    empty_rack = []
+    for i in range(0, 7):
+
 
